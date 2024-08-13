6ea69f619aac5a35667db0adccd695483f4ad28e
JONAAVILA <jona_1969@hotmail.com>
2024-08-13 09:01:05 -0300

    navbar fixed

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,4 @@
-import { Pressable, StyleSheet, View } from "react-native";
+import { Image, Pressable, StyleSheet, View } from "react-native";
 import AntDesign from '@expo/vector-icons/AntDesign';
 import { useNavigate } from "react-router-native";
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
@@ -11,7 +11,10 @@ export default function Navbar ({paht}){
             <Pressable>
                 <AntDesign onPress={()=>navigate(paht)} name="arrowleft" size={24} color="white"/>
             </Pressable>
+            <View style={styles.box_profile} >
                 <MaterialIcons name="notifications-none" size={24} color="white" />
+                <Image style={styles.profile} source={require('../../assets/profile.png')} />
+            </View>
         </View>
     )
 }
@@ -22,5 +25,16 @@ const styles = StyleSheet.create({
         flexDirection:'row',
         justifyContent:'space-between',
         width:400
+    },
+    profile:{
+        width:30,
+        height:30,
+        borderRadius:20,
+        marginLeft:10
+    },
+    box_profile:{
+        flexDirection:'row',
+        justifyContent:'center',
+        alignItems:'center'
     }
 })
