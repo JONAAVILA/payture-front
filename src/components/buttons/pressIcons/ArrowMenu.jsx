38a8fcd0b8263ed10a5bffbbd64dbd8d64fe057b
JONAAVILA <jona_1969@hotmail.com>
2024-08-15 12:26:57 -0300

    animated menu history

--- src/components/buttons/pressIcons/ArrowMenu.jsx
+++ src/components/buttons/pressIcons/ArrowMenu.jsx
@@ -2,10 +2,10 @@ import { Pressable } from "react-native";
 import MaterialIcons from '@expo/vector-icons/MaterialIcons';
 import theme from '../../../utils/theme';
 
-export default function ArrowMenu (){
+export default function ArrowMenu ({handleVisible}){
     return(
         <Pressable>
-            <MaterialIcons name="keyboard-arrow-down" size={40} color={theme.color.red} />
+            <MaterialIcons onPress={handleVisible} name="keyboard-arrow-down" size={40} color={theme.color.red} />
         </Pressable>
     )
 }
--- src/views/home/Home.jsx
+++ src/views/home/Home.jsx
@@ -3,13 +3,23 @@ import Navbar from "../../components/Navbar";
 import Publications from "../../components/cards/Publications";
 import Menu from "../../components/Menu";
 import History from "../../components/cards/History";
-import ArrowMenu from '../../components/buttons/pressIcons/ArrowMenu'
+import ArrowMenu from '../../components/buttons/pressIcons/ArrowMenu';
+import { AnimatePresence, MotiView } from "moti";
 
 import image from '../../../assets/image.png'
 import image2 from '../../../assets/image2.png'
 import image3 from '../../../assets/image3.png'
+import { useState } from "react";
 
 export default function Home (){
+
+    const [ visible,toggle ] = useState(false)
+
+    const handleVisible = ()=>{
+        if(visible) toggle(false)
+        if(!visible) toggle(true)
+    }
+
     return(
         <View style={styles.container_home} >
             <Navbar paht={'/usercreate'} />
@@ -19,14 +29,32 @@ export default function Home (){
                 <Publications image={image3} />
                 <Publications image={image} />
             </ScrollView>
-            <ArrowMenu/>
-            <ScrollView horizontal={true} style={styles.box_history}>
-                <History/>
-                <History/>
-                <History/>
-                <History/>
-                <History/>
-            </ScrollView>
+            <ArrowMenu handleVisible={handleVisible} />
+            <AnimatePresence>
+                {visible && <MotiView from={{
+                                          height:0
+                                      }}
+                                      animate={{
+                                          height:180
+                                      }}
+                                      exit={{
+                                          height:0
+                                      }}
+                                      transition={{
+                                        type: 'timing',
+                                        duration: 300,
+                                      }}
+                                      style={styles.box_history}
+                                      >
+                    <ScrollView horizontal={true}>
+                        <History/>
+                        <History/>
+                        <History/>
+                        <History/>
+                        <History/>
+                    </ScrollView>
+                </MotiView>}
+            </AnimatePresence>
             <Menu/>
         </View>
     )
@@ -41,5 +69,6 @@ const styles = StyleSheet.create({
     box_history:{
         paddingHorizontal:5,
         height:360,
+        overflow:'hidden'
     }
 })
