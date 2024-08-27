2be13051562ab2562099cddfc65bc6bb6d33a902
JONAAVILA <jona_1969@hotmail.com>
2024-08-27 12:57:29 -0300

    json web token aplieted to code request

--- src/adapters/FechtCode.jsx
+++ src/adapters/FechtCode.jsx
@@ -2,6 +2,7 @@ import axios from "axios";
 import { URL_FECHT_CODE } from '@env';
 
 export default async function FechtCode (email){
+    console.log(URL_FECHT_CODE)
     const response = await axios.post(URL_FECHT_CODE,{email:email})
     return response.data
 }
